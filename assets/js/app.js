099d70af02070be8e15e999b3934579eaec8e374
julien-gabard <juliengabard@hotmail.fr>
2020-08-22 00:50:12 +0200

    restor apiBaseUrl

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,7 +42,7 @@ let app = {
     // Méthodes de récupération des datas depuis l'API *lumen(laravel)*
     //--------------------------------------------------------------------
 
-    apiBaseUrl: 'http://54.167.206.247/Todolist-api/',// <- l'URL de l'API lumen
+    apiBaseUrl: 'http://localhost:8080/',// <- l'URL de l'API lumen
 
     fetchData: function() {// <- réunie l'appel a l'API de catégorie et tâche
         app.fetchCategories();
